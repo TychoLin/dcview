a8c11f6e4751533c96ff9c3a9dce2625e18a5522
Tycho Lin <tycho.lin@gmail.com>
2014-07-27 17:16:52 +0800

    first dev rwd navbar with bootstrap

---
+++ edm/rwd_edm_list.php
@@ -0,0 +1,47 @@
+<?php
+require_once("common.inc.php");
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="../bootstrap/3.2.0/css/bootstrap.css">
+	<link rel="stylesheet" href="../bootstrap/3.2.0/css/bootstrap-theme.css">
+	<style type="text/css">
+	@media (min-width: 992px) {
+		.container {
+			width: 750px;
+		}
+	}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<nav class="navbar navbar-default" role="navigation">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<img src="img/logo.gif">
+				</div>
+				<div class="navbar-collapse collapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="active"><a href="./">討論區</a></li>
+						<li><a href="#">作品發表</a></li>
+						<li><a href="#">文章總覽</a></li>
+						<li><a href="#">test</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</div>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src ="../bootstrap/3.2.0/js/bootstrap.js"></script>
+</body>
+</html>
