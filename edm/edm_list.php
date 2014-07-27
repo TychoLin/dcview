530ac6fd12f0addc554a16162352808010e4fc54
Tycho Lin <tycho.lin@gmail.com>
2014-07-27 22:03:25 +0800

    create edm list template with pure css framework

--- edm/edm_list.php
+++ edm/edm_list.php
@@ -1,5 +1,179 @@
-<?php
+﻿<?php
 require_once("common.inc.php");
 
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+	<!--[if lte IE 8]>
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/grids-responsive-old-ie-min.css">
+	<![endif]-->
+	<!--[if gt IE 8]><!-->
+	<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/grids-responsive-min.css">
+	<!--<![endif]-->
+	<style type="text/css">
+	.conainter {
+		width: 48em;
+		margin: 0 auto;
+	}
+
+	.header {
+		background: url("img/top_bg.gif") repeat-x;
+		margin-bottom: 14px;
+	}
+
+	.nav {
+		margin: 0.8em 0 0 0;
+		padding: 0px;
+		font-size: 0.8em;
+		list-style: none;
+	}
+
+	.nav li {
+		float: left;
+		margin-left: 0.5em;
+		padding-left: 0.5em;
+		border-left: 1px solid rgb(200, 200, 200);
+	}
+
+	.nav li:first-child {
+		border-left: 0px;
+		margin-left: 0px;
+		padding-left: 0px;
+	}
+
+	.navbar-right {
+		float: right;
+	}
+
+	.jumbotron {
+		border: 1px solid #d8d8d8;
+		padding: 12px;
+		font-size: 0.8em;
+	}
+
+	.jumbotron h3 {
+		width: 380px;
+		font-size: 1.3em;
+	}
+
+	.cell {
+		padding: 0.8em;
+		font-size: 0.8em;
+	}
+
+	.volume {
+		font-size: 1.3em;
+		font-weight: bold;
+	}
+
+	.title {
+		font-size: 1.3em;
+		font-weight: bold;
+		display: block;
+		margin-top: 10px;
+	}
+
+	.footer {
+		padding: 0.8em;
+		background: url("img/copy_bg.gif") repeat-x;
+	}
+	</style>
+</head>
+<body>
+	<div class="conainter">
+		<div class="header">
+			<img src="img/logo.gif">
+			<ul class="nav navbar-right">
+				<li><a href="#">討論區</a></li>
+				<li><a href="#">作品發表</a></li>
+				<li><a href="#">文章總覽</a></li>
+			</ul>
+		</div>
+		<div class="jumbotron">
+			<div class="pure-g">
+				<div class="pure-u-1 pure-u-md-7-24"><img src="" width="200" height="130"></div>
+				<div class="pure-u-1 pure-u-md-17-24">
+					<span style="float: right;">2014/06/25 出刊</span>
+					<h3>第517期 - 長焦望遠鏡的新選擇： Kowa螢石鏡頭500mm F5.6</h3>
+					<ul>
+						<li>[ 焦點新聞追蹤 ] LG Pocket photo 3.0相印機 即編、即印、即貼、即分享!</li>
+						<li>[ 達人部落精選 ] 林百貨</li>
+						<li>[ 攝影好讀 ] 如何拍攝靜物與建築？</li>
+					</ul>
+					<a href="#" style="float: right;">閱讀內容</a>
+				</div>
+			</div>
+		</div>
+		<hr style="margin: 14px 0; border: 1px dashed #d8d8d8;">
+		<div class="pure-g">
+			<div class="pure-u-1 pure-u-md-1-2">
+				<div class="pure-g cell">
+					<div class="pure-u-1 pure-u-md-7-24"><img src="" width="100" height="65"></div>
+					<div class="pure-u-1 pure-u-md-17-24">
+						<span style="float: right;">2014/06/25 出刊</span>
+						<span class="volume">第516期</span>
+						<span class="title">紅外線攝影的好幫手-STC UV-IR CUT濾鏡運用分享</span>
+						<a href="#" style="float: right;">閱讀內容</a>
+					</div>
+				</div>
+			</div>
+			<div class="pure-u-1 pure-u-md-1-2">
+				<div class="pure-g cell">
+					<div class="pure-u-1 pure-u-md-7-24"><img src="" width="100" height="65"></div>
+					<div class="pure-u-1 pure-u-md-17-24">
+						<span style="float: right;">2014/06/25 出刊</span>
+						<span class="volume">第516期</span>
+						<span class="title">紅外線攝影的好幫手-STC UV-IR CUT濾鏡運用分享</span>
+						<a href="#" style="float: right;">閱讀內容</a>
+					</div>
+				</div>
+			</div>
+			<div class="pure-u-1 pure-u-md-1-2">
+				<div class="pure-g cell">
+					<div class="pure-u-1 pure-u-md-7-24"><img src="" width="100" height="65"></div>
+					<div class="pure-u-1 pure-u-md-17-24">
+						<span style="float: right;">2014/06/25 出刊</span>
+						<span class="volume">第516期</span>
+						<span class="title">紅外線攝影的好幫手-STC UV-IR CUT濾鏡運用分享</span>
+						<a href="#" style="float: right;">閱讀內容</a>
+					</div>
+				</div>
+			</div>
+			<div class="pure-u-1 pure-u-md-1-2">
+				<div class="pure-g cell">
+					<div class="pure-u-1 pure-u-md-7-24"><img src="" width="100" height="65"></div>
+					<div class="pure-u-1 pure-u-md-17-24">
+						<span style="float: right;">2014/06/25 出刊</span>
+						<span class="volume">第516期</span>
+						<span class="title">紅外線攝影的好幫手-STC UV-IR CUT濾鏡運用分享</span>
+						<a href="#" style="float: right;">閱讀內容</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<hr style="margin: 14px 0; border: 1px dashed #d8d8d8;">
+		<div class="footer">
+			<div class="pure-g">
+				<div class="pure-u-1 pure-u-md-1-3">
+					<img src="img/logo_dcview_edm_f.gif">
+				</div>
+				<div class="pure-u-1 pure-u-md-2-3">
+					<ul class="nav">
+						<li><a href="#">服務說明與客服中心</a></li>
+						<li><a href="#">整合行銷服務</a></li>
+						<li><a href="#">行銷合作</a></li>
+						<li><a href="#">廣告刊登</a></li>
+					</ul>
+					<div style="clear: both;"></div>
+					<p>Copyright © 2002-2014 DCView All Rights Reserved.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
